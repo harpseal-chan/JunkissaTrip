b7adc472143ac8baa8f0b91501c50fedba0f181d
harpseal-chan <4062yoshi@gmail.com>
2022-07-21 07:23:07 +0900

    add Shop seeds

---
+++ db/fixtures/01_shop.rb
@@ -0,0 +1,71 @@
+Shop.seed do |s|
+  s.id = 1
+  s.name = '喫茶ニカイ'
+  s.address = '東京都台東区谷中6-3-8'
+  s.phone = '03-5834-2922'
+  s.opening = '11:00 - 18:00'
+  s.closed = '水曜'
+end
+
+Shop.seed do |s|
+  s.id = 2
+  s.name = '喫茶まりも'
+  s.address = '神奈川県川崎市中原区新丸子東1-785'
+  s.phone = '044-422-3570'
+  s.opening = '10:00 - 17:00'
+  s.closed = '年中無休'
+end
+
+Shop.seed do |s|
+  s.id = 3
+  s.name = '珈琲道場 侍'
+  s.address = '東京都江東区亀戸6-57-22 サンポービル 2F'
+  s.phone = '03-3638-4003'
+  s.opening = '金土 8:00 - 23:00 / 月火水木 8:00 - 0:00'
+  s.closed = '日曜'
+end
+
+Shop.seed do |s|
+  s.id = 4
+  s.name = 'ニューYC'
+  s.address = '大阪府大阪市北区角田町9-26 新梅田食道街'
+  s.phone = '06-6312-0709'
+  s.opening = '5:30 - 23:00'
+  s.closed = '年中無休'
+end
+
+Shop.seed do |s|
+  s.id = 5
+  s.name = '喫茶&Bar King of Kings'
+  s.address = '大阪市北区梅田1-3-1 大阪駅前第1ビルB1F'
+  s.phone = '06-6345-3100'
+  s.opening = '月～土 12:00 - 23:00'
+  s.closed = '日曜・祝日・祝前日'
+end
+
+Shop.seed do |s|
+  s.id = 6
+  s.name = 'マヅラ喫茶店'
+  s.address = '大阪市北区梅田1-3-1 大阪駅前第1ビルB1F'
+  s.phone = '06-6345-3400'
+  s.opening = '月～土 9:00 - 20:30'
+  s.closed = '日曜・祝日'
+end
+
+Shop.seed do |s|
+  s.id = 7
+  s.name = 'イノダコーヒ 本店'
+  s.address = '京都市中京区堺町通三条下ル道祐町140'
+  s.phone = '075-221-0507'
+  s.opening = '7:00 - 18:00'
+  s.closed = '年中無休'
+end
+
+Shop.seed do |s|
+  s.id = 8
+  s.name = '喫茶ドレミ'
+  s.address = '大阪府大阪市浪速区恵美須東1-18-8'
+  s.phone = '06-6643-6076'
+  s.opening = '10:00 - 18:00'
+  s.closed = '不定休'
+end
