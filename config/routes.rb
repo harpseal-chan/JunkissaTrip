c3571f55c0c97c234d0e481f59dffeebd05a3b50
harpseal-chan <4062yoshi@gmail.com>
2022-07-23 16:35:24 +0900

    add shop index layout

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users, except: [:index]
+  resources :shops, only: [:index, :show]
 end
