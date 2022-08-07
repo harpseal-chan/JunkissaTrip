2729deaa295e3ead55c05dc9b9ae83877c4b81d0
harpseal-chan <4062yoshi@gmail.com>
2022-08-07 17:36:35 +0900

    add search-bar test

--- spec/system/searches_spec.rb
+++ spec/system/searches_spec.rb
@@ -9,20 +9,41 @@ RSpec.describe "Searches", type: :system do
     visit shops_path
   end
 
-  context '店舗名で検索' do
-    it '条件に合う店舗のみ表示されること' do
-      fill_in 'q[name_cont]', with: match_shop.name
-      page.first(".header-search-btn").click
-      expect(page).to have_content match_shop.name
-      expect(page).not_to have_content not_match_shop.name
+  describe 'ヘッダーの検索バー' do
+    before do
+      visit shops_path
+    end
+
+    context '店舗名で検索' do
+      it '条件に合う店舗のみ表示されること' do
+        fill_in 'q[name_cont]', with: match_shop.name
+        page.first(".header-search-btn").click
+        expect(page).to have_content match_shop.name
+        expect(page).not_to have_content not_match_shop.name
+      end
+    end
+
+    context '条件に一致する店舗がない場合' do
+      it '#shop-not-foundが表示されること' do
+        fill_in 'q[name_cont]', with: 'not-found'
+        page.first(".header-search-btn").click
+        expect(page).to have_selector '#shop-not-found'
+      end
     end
   end
 
-  context '条件に一致する店舗がない場合' do
-    it '#shop-not-foundが表示されること' do
-      fill_in 'q[name_cont]', with: 'not-found'
-      page.first(".header-search-btn").click
-      expect(page).to have_selector '#shop-not-found'
+  describe 'トップページの検索バー' do
+    before do
+      visit root_path
+    end
+
+    context '店舗名で検索' do
+      it '条件に合う店舗のみ表示されること' do
+        fill_in 'q[name_cont]', with: match_shop.name
+        page.first(".top-search-btn").click
+        expect(page).to have_content match_shop.name
+        expect(page).not_to have_content not_match_shop.name
+      end
     end
   end
 end
