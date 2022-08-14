7e234977a13ec830b8f22dd40187e80fb649b93e
harpseal-chan <4062yoshi@gmail.com>
2022-08-14 09:13:01 +0900

    add nginx

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ rm -f /app/tmp/pids/server.pid
 
 if [ "${RAILS_ENV}" = "production" ]
 then
-    bundle exec rails assets:precompile
+    # bundle exec rails assets:precompile
     bundle exec rails db:migrate
     bundle exec rails db:seed_fu
 fi
