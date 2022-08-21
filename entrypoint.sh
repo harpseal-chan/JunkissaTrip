a2e560ae2aad0f89be61088cf42b92c982afa4e2
harpseal-chan <4062yoshi@gmail.com>
2022-08-21 21:44:20 +0900

    本番環境用に設定ファイル修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,9 @@ rm -f /app/tmp/pids/server.pid
 
 if [ "${RAILS_ENV}" = "production" ]
 then
-    # bundle exec rails assets:precompile
-    bundle exec rails db:migrate
-    bundle exec rails db:seed_fu
+    bundle exec rails assets:precompile RAILS_ENV=production
+    bundle exec rails db:migrate RAILS_ENV=production
+    bundle exec rails db:seed_fu RAILS_ENV=production
 fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
