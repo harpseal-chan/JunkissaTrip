f4e212414c1e6b9c1377edc2493a04cfe049eca7
harpseal-chan <4062yoshi@gmail.com>
2022-08-25 20:34:33 +0900

    ShopFeatureモデルのバリデーションテスト追加

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -2,6 +2,6 @@ class Feature < ApplicationRecord
   has_many :shop_features, dependent: :destroy
   has_many :shops, through: :shop_features
 
-  validates :icon_name, presence: true, length: { maximum: 50 }
-  validates :detail, presence: true, length: { maximum: 255 }
+  validates :icon_name, presence: true, length: { maximum: 50 }, uniqueness: true
+  validates :detail, presence: true, length: { maximum: 255 }, uniqueness: true
 end
