dd55fb0404c6347f2338630a48271b4125ea22c3
harpseal-chan <4062yoshi@gmail.com>
2022-08-27 14:00:40 +0900

    アイコンの種類変更、店舗詳細ページにアイコン追加

--- db/fixtures/02_feature.rb
+++ db/fixtures/02_feature.rb
@@ -1,10 +1,10 @@
 Feature.seed(
-  { :id => 1, :icon_name => 'coffee-beans', :detail => 'コーヒーにこだわりあり' },
-  { :id => 2, :icon_name => 'pancakes', :detail => 'スイーツメニューが充実' },
-  { :id => 3, :icon_name => 'fork-knife', :detail => '食事メニューが充実' },
-  { :id => 4, :icon_name => 'glass', :detail => 'ドリンクメニューが充実' },
-  { :id => 5, :icon_name => 'person', :detail => '一人でも入りやすい' },
-  { :id => 6, :icon_name => 'comments', :detail => '話しやすい雰囲気' },
-  { :id => 7, :icon_name => 'book-open-reader', :detail => '静かな雰囲気' },
-  { :id => 8, :icon_name => 'couch', :detail => 'インテリアがおしゃれ' },
+  { :id => 1, :icon_name => 'coffee-bean.svg', :detail => 'コーヒーにこだわりあり' },
+  { :id => 2, :icon_name => 'cake.svg', :detail => 'スイーツメニューが充実' },
+  { :id => 3, :icon_name => 'meal.svg', :detail => '食事メニューが充実' },
+  { :id => 4, :icon_name => 'drink.svg', :detail => 'ドリンクメニューが充実' },
+  { :id => 5, :icon_name => 'person.svg', :detail => '一人でも入りやすい' },
+  { :id => 6, :icon_name => 'conversation.svg', :detail => '話しやすい雰囲気' },
+  { :id => 7, :icon_name => 'reading.svg', :detail => '静かな雰囲気' },
+  { :id => 8, :icon_name => 'sofa.svg', :detail => 'インテリアがおしゃれ' },
 )
