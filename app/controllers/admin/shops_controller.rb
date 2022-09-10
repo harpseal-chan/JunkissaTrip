4aac78828d0f9aa8b461058d6d951b372b4b4edc
harpseal-chan <4062yoshi@gmail.com>
2022-09-10 18:18:09 +0900

    Shopコントローラのupdateアクション実装 #88

--- app/controllers/admin/shops_controller.rb
+++ app/controllers/admin/shops_controller.rb
@@ -6,4 +6,20 @@ class Admin::ShopsController < ApplicationController
   def edit
     @shop = Shop.find(params[:id])
   end
+
+  def update
+    @shop = Shop.find(params[:id])
+    if @shop.update(shop_params)
+      flash[:success] = '店舗情報を更新しました'
+      redirect_to admin_shops_url
+    else
+      render :edit
+    end
+  end
+
+  private
+
+    def shop_params
+      params.require(:shop).permit(:name, :address, :phone, :opening, :closed)
+    end
 end
