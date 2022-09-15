12d6e5c75977a61203c5eeb2effd4e0512487e11
harpseal-chan <4062yoshi@gmail.com>
2022-09-15 19:38:57 +0900

    Shopモデルのimageカラムと店舗画像を削除 #88

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_10_092843) do
+ActiveRecord::Schema.define(version: 2022_09_15_102310) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -84,7 +84,6 @@ ActiveRecord::Schema.define(version: 2022_09_10_092843) do
     t.string "closed"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "image"
     t.float "latitude"
     t.float "longitude"
   end
