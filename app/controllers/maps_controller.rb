98bd77b47976c8c814c97c481acf883e8f9377e4
harpseal-chan <4062yoshi@gmail.com>
2022-10-06 21:05:47 +0900

    slider関連のエラーを修正

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,4 +1,8 @@
 class MapsController < ApplicationController
   def search
+    default_lat = 35.681236
+    default_lng = 139.767125
+    gon.lat = default_lat
+    gon.lng = default_lng
   end
 end
