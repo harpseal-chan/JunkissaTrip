f94fcb49071c127b62b67c920d1b8bd8ec27c8e4
harpseal-chan <4062yoshi@gmail.com>
2022-10-12 07:01:38 +0900

    マップ検索ボタンを２回クリックした際に緯度経度が空になる不具合を修正

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -4,10 +4,12 @@ class MapsController < ApplicationController
       gon.lat = geo_params[:latitude].to_f
       gon.lng = geo_params[:longitude].to_f
     else
-      # default lat_lng
+      # default value
       gon.lat = 35.681236
       gon.lng = 139.767125
     end
+    @latitude = gon.lat
+    @longitude = gon.lng
     radius = 1.5
     @shops = Shop.all.within(radius, origin: [gon.lat, gon.lng]).by_distance(origin: [gon.lat, gon.lng]).preload(:features, images_attachments: :blob)
     gon.shops = @shops
