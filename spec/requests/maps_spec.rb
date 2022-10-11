1909691aeffcb346da8990b865ffcb868acd7235
harpseal-chan <4062yoshi@gmail.com>
2022-10-11 19:21:26 +0900

    店舗詳細ページのマップをMapsEmbedAPIに変更

--- spec/requests/maps_spec.rb
+++ spec/requests/maps_spec.rb
@@ -20,17 +20,17 @@ RSpec.describe "Maps", type: :request do
       let!(:match_shop) { FactoryBot.create(:shop1) }
       let!(:not_match_shop) { FactoryBot.create(:shop2) }
 
-      context 'マーカーの位置から半径1km以内に店舗がある場合' do  
+      context 'マーカーの位置から半径1km以内に店舗がある場合' do
         it '条件に合う店舗が表示されること' do
-          get maps_path, params: { lat_lng: { latitude: 35.723, longitude: 139.7687 }}
+          get maps_path, params: { lat_lng: { latitude: 35.723, longitude: 139.7687 } }
           expect(response.body).to include match_shop.name
           expect(response.body).not_to include not_match_shop.name
         end
       end
 
-      context 'マーカーの位置から半径1km以内に店舗がない場合' do  
+      context 'マーカーの位置から半径1km以内に店舗がない場合' do
         it 'エラーメッセージが表示されること' do
-          get maps_path, params: { lat_lng: { latitude: 0, longitude: 0 }}
+          get maps_path, params: { lat_lng: { latitude: 0, longitude: 0 } }
           expect(response.body).to include '条件に合う純喫茶がありません'
         end
       end
