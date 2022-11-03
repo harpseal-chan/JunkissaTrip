67e3cbbe2b6ed4c9c953a3c53a0d90a32441fb2d
harpseal-chan <4062yoshi@gmail.com>
2022-11-03 11:49:41 +0900

    表記を「探す」に統一

--- spec/system/maps_spec.rb
+++ spec/system/maps_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe "Maps", type: :system do
     driven_by(:rack_test)
   end
 
-  context 'マップ検索ボタンを２回クリックした場合' do
+  context 'マップから探すボタンを２回クリックした場合' do
     it '同じ位置から店舗を検索できること' do
       visit maps_path
-      click_button '周辺の純喫茶をさがす'
-      click_button '周辺の純喫茶をさがす'
+      click_button '周辺の純喫茶を探す'
+      click_button '周辺の純喫茶を探す'
       expect(page).to have_content shop.name
     end
   end
